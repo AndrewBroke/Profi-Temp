95da1b5788f8e33e08b1ebb32b1ad05c3740a42e
AndrewBroke <kuzukov-work@yandex.ru>
2025-02-08 19:16:16 +0700

    Update MainMenu.cs

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    /// <summary>
+    /// Loads the game scene
+    /// </summary>
+    /// <param name="sceneName">The name of the game scene</param>
+    public void LoadGame(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
     /// <summary>
     /// Exit game in the build 
     /// And pause in the Unity Editor
